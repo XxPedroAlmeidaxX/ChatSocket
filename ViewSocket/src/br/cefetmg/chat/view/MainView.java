2cdc238e19cc250bab318e9a8b6cff6b754af2ed
Vitor Rodarte <vitorricoy@yahoo.com.br>
2017-10-05 10:09:56 -0300

    Inicio classe principal

---
+++ ViewSocket/src/br/cefetmg/chat/view/MainView.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetmg.chat.view;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Aluno
+ */
+public class MainView extends Application {
+    
+    private Stage primaryStage;
+    private Pane rootLayout;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("comeco2");
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Logar Chat");
+        showLogin();
+    }
+    
+    private void showLogin(){
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainView.class.getResource("landPage.fxml"));
+            rootLayout = (VBox) loader.load();
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
