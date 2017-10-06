ae2ac2dacf010145b895029c42114f9ee0406a4e
vitorricoy <vitorricoy@yahoo.com.br>
2017-10-05 22:23:45 -0300

    Mais da home

--- ViewSocket/src/br/cefetmg/chat/view/MainView.java
+++ ViewSocket/src/br/cefetmg/chat/view/MainView.java
@@ -5,13 +5,16 @@
  */
 package br.cefetmg.chat.view;
 
+import br.cefetmg.chat.domain.Message;
 import br.cefetmg.chat.domain.Room;
 import br.cefetmg.chat.domain.User;
 import br.cefetmg.chat.exception.BusinessException;
 import br.cefetmg.chat.exception.ConnectionException;
 import br.cefetmg.chat.implementation.connection.ConnectionManager;
+import br.cefetmg.chat.implementation.service.MessageBusiness;
 import br.cefetmg.chat.implementation.service.RoomBusiness;
 import br.cefetmg.chat.interfaces.connection.IConnection;
+import br.cefetmg.chat.interfaces.service.IMessageBusiness;
 import br.cefetmg.chat.interfaces.service.IRoomBusiness;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,14 +27,17 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javax.swing.event.HyperlinkEvent;
 
@@ -56,29 +62,13 @@ public class MainView extends Application {
         } catch (ConnectionException ex) {
             System.out.println("Erro: " + ex.getMessage());
         }
-        salas = new ArrayList<>();
-        ArrayList<User> list = new ArrayList<>();
-        list.add(new User());
-        Room r1 = new Room(new Long(0), "sala 1", true, " ", list);
-        list = new ArrayList<>();
-        list.add(new User());
-        list.add(new User());
-        Room r2 = new Room(new Long(1), "sala 2", true, " ", list);
-        list = new ArrayList<>();
-        list.add(new User());
-        list.add(new User());
-        list.add(new User());
-        Room r3 = new Room(new Long(2), "sala 3", true, " ", list);
-        salas.add(r1);
-        salas.add(r2);
-        salas.add(r3);
         showHome();
     }
     
     public void showHome(){
         try {
-            //IRoomBusiness roomB = new RoomBusiness();
-            //salas = roomB.getAllRoom();
+            IRoomBusiness roomB = new RoomBusiness();
+            salas = roomB.getAllRoom();
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainView.class.getResource("Home.fxml"));
             rootLayout = (VBox) loader.load();
@@ -96,16 +86,21 @@ public class MainView extends Application {
                 b.setMinWidth(salasDisponiveis.getWidth());
                 b.setTextFill(Paint.valueOf("white")); 
                 b.setStyle("-fx-background-color: #660000");
-                b.setId(r.getIdRoom().toString());
+                b.setId("sala"+r.getIdRoom().toString());
                 b.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
-                        System.out.println(((Button)event.getSource()).getId());
+                        Long id = Long.parseLong(((Button)event.getSource()).getId().substring(4));
+                        try {
+                            loadRoom(roomB.getRoomById(id));
+                        } catch (BusinessException ex) {
+                            System.out.println("Erro: " + ex.getMessage());
+                        }
                     }
                 });
                 filhas.add(b);
             }
-        } catch (IOException  e) {
+        } catch (IOException | BusinessException e) {
             System.out.println("Erro: " + e.getMessage());
         }
     }
@@ -123,6 +118,58 @@ public class MainView extends Application {
         } catch (IOException e) {
             System.out.println("Erro: " + e.getMessage());
         }
+    }
+    
+    public void loadRoom(Room r) throws BusinessException{
+         FlowPane mensagens = (FlowPane)rootLayout.lookup("#mensagensContainer");
+         IMessageBusiness busi = new MessageBusiness();
+         List<Message> mensagensList = busi.getMessagesByRoom(r);
+         ObservableList filhasMensagens = mensagens.getChildren();
+         mensagens.setVgap(10);
+         filhasMensagens.remove(0, filhasMensagens.size());
+         for(Message m:mensagensList){
+             Label lMsg = new Label();
+             lMsg.setId("mensagem"+m.getIdMessage());
+             lMsg.setText(m.getTextMessage());
+             lMsg.setAlignment(Pos.BOTTOM_LEFT);
+             lMsg.setMinWidth(mensagens.getWidth());
+             if(m.getUser().getIpUser()==logado.getIpUser() && m.getUser().getNameUser().equals(logado.getNameUser())){
+                 lMsg.setStyle("-fx-background-color: #e0e0e0");
+                 lMsg.setTextFill(Paint.valueOf("black")); 
+             }else{
+                 lMsg.setStyle("-fx-background-color: #42e8f4");
+                 lMsg.setTextFill(Paint.valueOf("white")); 
+             }
+             Label lUsuario = new Label();
+             if(m.getUser().getIpUser()==logado.getIpUser() && m.getUser().getNameUser().equals(logado.getNameUser())){
+                 lUsuario.setStyle("-fx-background-color: #e0e0e0");
+                 lUsuario.setTextFill(Paint.valueOf("black")); 
+             }else{
+                 lUsuario.setStyle("-fx-background-color: #42e8f4");
+                 lUsuario.setTextFill(Paint.valueOf("white")); 
+             }
+             lUsuario.setAlignment(Pos.BOTTOM_LEFT);
+             lUsuario.setMinWidth(mensagens.getWidth());
+             lUsuario.setId("usMsg-"+m.getUser().getIpUser()+"-"+m.getUser().getNameUser());
+             lUsuario.setText(m.getUser().getNameUser());
+             lUsuario.setFont(Font.font ("System", 10));
+             filhasMensagens.add(lMsg);
+             filhasMensagens.add(lUsuario);
+         }
+         FlowPane logados = (FlowPane)rootLayout.lookup("#logadosContainer");
+         ObservableList filhasLogados = logados.getChildren();
+         logados.setVgap(15);
+         filhasLogados.remove(0, filhasMensagens.size());
+         for(User u : r.getUsuarios()){
+             if(u.getIpUser()!=logado.getIpUser() && !u.getNameUser().equals(logado.getNameUser())){
+                 Label l = new Label();
+                 l.setId("usLog-"+u.getIpUser()+"-"+u.getNameUser());
+                 l.setText(u.getNameUser());
+                 l.setMinWidth(logados.getWidth());
+                 l.setStyle("-fx-background-color: #660000");
+                 filhasLogados.add(l);
+             }
+         }
     }
 
     public void showRoomMaker() {
