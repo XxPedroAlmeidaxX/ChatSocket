fb07a28c37e70f56f2656013b99dcac55ce5fd56
Adalberto <adalbertob.vieira@gmail.com>
2017-10-08 20:17:36 -0300

    teste Message

--- ClientSocket/src/br/cefetmg/chat/exception/BusinessException.java
+++ ClientSocket/src/br/cefetmg/chat/exception/BusinessException.java
@@ -4,4 +4,8 @@ public class BusinessException extends Exception{
     public BusinessException(String msg){
         super(msg);
     }
+
+    public BusinessException() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
